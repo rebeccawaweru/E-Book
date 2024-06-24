0c7e2b6054979389a6915aaec24d3b15ce7e3f17
rebeccawaweru <wawerur95@gmail.com>
2024-06-24 11:39:31 +0300

    meta data update

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,12 +5,17 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Kill Your Enemies by Abel Marite",
-  description: "Published and now available in both softcopy and hardcopy format. Make your purchase today!",
+  description: "Published and now available. Make your purchase today!",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+       <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
